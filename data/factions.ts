28d0139f53eb7340cb2026ecfe5e87e41b0a1445
Qu4 <lkauertz@gmx.de>
2026-07-05 23:15:35 +0200

    added more options

--- data/factions.ts
+++ data/factions.ts
@@ -2,33 +2,403 @@ import type { GameOption } from "@/types/game";
 
 export const factions: GameOption[] = [
   {
-    name: "Akatsuki",
-    rarity: "Legendary",
-    image: "/images/factions/akatsuki.webp",
-  },
-  {
-    name: "Straw Hat Pirates",
-    rarity: "Legendary",
-    image: "/images/factions/straw-hat-pirates.webp",
-  },
-  {
-    name: "Survey Corps",
-    rarity: "Epic",
+    name: "Survey-Corps",
+    rarity: "Rare",
     image: "/images/factions/survey-corps.webp",
   },
   {
-    name: "Demon Slayer Corps",
+    name: "Garrison-Regiment",
+    rarity: "Common",
+    image: "/images/factions/garrison-regiment.webp",
+  },
+  {
+    name: "Japanese-Task-Force",
     rarity: "Rare",
+    image: "/images/factions/japanese-task-force.webp",
+  },
+  {
+    name: "Yotsuba-Group",
+    rarity: "Common",
+    image: "/images/factions/yotsuba-group.webp",
+  },
+  {
+    name: "State-Alchemists",
+    rarity: "Epic",
+    image: "/images/factions/state-alchemists.webp",
+  },
+  {
+    name: "Chimera-Soldiers",
+    rarity: "Common",
+    image: "/images/factions/chimera-soldiers.webp",
+  },
+  {
+    name: "S-Class-Heroes",
+    rarity: "Epic",
+    image: "/images/factions/s-class-heroes.webp",
+  },
+  {
+    name: "Tanktop-Army",
+    rarity: "Common",
+    image: "/images/factions/tanktop-army.webp",
+  },
+  {
+    name: "Demon-Slayer-Corps",
+    rarity: "Epic",
     image: "/images/factions/demon-slayer-corps.webp",
   },
   {
-    name: "Marines",
+    name: "Twelve-Kizuki",
     rarity: "Common",
-    image: "/images/factions/marines.webp",
+    image: "/images/factions/twelve-kizuki.webp",
   },
   {
-    name: "Lone Mercenary",
+    name: "UA-High-Hero-Course",
+    rarity: "Legendary",
+    image: "/images/factions/ua-high-hero-course.webp",
+  },
+  {
+    name: "League-of-Villains",
     rarity: "Common",
-    image: "/images/factions/lone-mercenary.webp",
+    image: "/images/factions/league-of-villains.webp",
+  },
+  {
+    name: "Knights-of-the-Blood-Oath",
+    rarity: "Rare",
+    image: "/images/factions/knights-of-the-blood-oath.webp",
+  },
+  {
+    name: "Laughing-Coffin",
+    rarity: "Common",
+    image: "/images/factions/laughing-coffin.webp",
+  },
+  {
+    name: "Hunter-Association",
+    rarity: "Epic",
+    image: "/images/factions/hunter-association.webp",
+  },
+  {
+    name: "Phantom-Troupe",
+    rarity: "Rare",
+    image: "/images/factions/phantom-troupe.webp",
+  },
+  {
+    name: "Konohagakure-Shinobi",
+    rarity: "Epic",
+    image: "/images/factions/konohagakure-shinobi.webp",
+  },
+  {
+    name: "Akatsuki",
+    rarity: "Rare",
+    image: "/images/factions/akatsuki.webp",
+  },
+  {
+    name: "CCG-Investigators",
+    rarity: "Rare",
+    image: "/images/factions/ccg-investigators.webp",
+  },
+  {
+    name: "Aogiri-Tree",
+    rarity: "Common",
+    image: "/images/factions/aogiri-tree.webp",
+  },
+  {
+    name: "Tokyo-Jujutsu-High",
+    rarity: "Epic",
+    image: "/images/factions/tokyo-jujutsu-high.webp",
+  },
+  {
+    name: "Kyoto-Jujutsu-High",
+    rarity: "Rare",
+    image: "/images/factions/kyoto-jujutsu-high.webp",
+  },
+  {
+    name: "Straw-Hat-Pirates",
+    rarity: "Rare",
+    image: "/images/factions/straw-hat-pirates.webp",
+  },
+  {
+    name: "Buggy-Pirates",
+    rarity: "Common",
+    image: "/images/factions/buggy-pirates.webp",
+  },
+  {
+    name: "Black-Knights",
+    rarity: "Rare",
+    image: "/images/factions/black-knights.webp",
+  },
+  {
+    name: "Britannian-Army",
+    rarity: "Common",
+    image: "/images/factions/britannian-army.webp",
+  },
+  {
+    name: "Emilia-Camp",
+    rarity: "Rare",
+    image: "/images/factions/emilia-camp.webp",
+  },
+  {
+    name: "Royal-Knights-of-Lugunica",
+    rarity: "Common",
+    image: "/images/factions/royal-knights-of-lugunica.webp",
+  },
+  {
+    name: "Body-Improvement-Club",
+    rarity: "Common",
+    image: "/images/factions/body-improvement-club.webp",
+  },
+  {
+    name: "Claw",
+    rarity: "Common",
+    image: "/images/factions/claw.webp",
+  },
+  {
+    name: "Yatos-Regalia",
+    rarity: "Common",
+    image: "/images/factions/yatos-regalia.webp",
+  },
+  {
+    name: "Heavens-Shinki",
+    rarity: "Rare",
+    image: "/images/factions/heavens-shinki.webp",
+  },
+  {
+    name: "Night-Raid",
+    rarity: "Rare",
+    image: "/images/factions/night-raid.webp",
+  },
+  {
+    name: "Jaegers",
+    rarity: "Common",
+    image: "/images/factions/jaegers.webp",
+  },
+  {
+    name: "Class-3E",
+    rarity: "Rare",
+    image: "/images/factions/class-3e.webp",
+  },
+  {
+    name: "Ministry-of-Defense-Assassination-Team",
+    rarity: "Common",
+    image: "/images/factions/ministry-of-defense-assassination-team.webp",
+  },
+  {
+    name: "Gotei-13",
+    rarity: "Epic",
+    image: "/images/factions/gotei-13.webp",
+  },
+  {
+    name: "Xcution",
+    rarity: "Common",
+    image: "/images/factions/xcution.webp",
+  },
+  {
+    name: "Seven-Deadly-Sins",
+    rarity: "Epic",
+    image: "/images/factions/seven-deadly-sins.webp",
+  },
+  {
+    name: "Holy-Knights-of-Liones",
+    rarity: "Common",
+    image: "/images/factions/holy-knights-of-liones.webp",
+  },
+  {
+    name: "Goldy-Pond-Resistance",
+    rarity: "Common",
+    image: "/images/factions/goldy-pond-resistance.webp",
+  },
+  {
+    name: "Lambda-7214-Subjects",
+    rarity: "Common",
+    image: "/images/factions/lambda-7214-subjects.webp",
+  },
+  {
+    name: "True-Cross-Order",
+    rarity: "Common",
+    image: "/images/factions/true-cross-order.webp",
+  },
+  {
+    name: "Illuminati",
+    rarity: "Common",
+    image: "/images/factions/illuminati.webp",
+  },
+  {
+    name: "Public-Safety-Devil-Hunters",
+    rarity: "Rare",
+    image: "/images/factions/public-safety-devil-hunters.webp",
+  },
+  {
+    name: "Special-Division-4",
+    rarity: "Common",
+    image: "/images/factions/special-division-4.webp",
+  },
+  {
+    name: "First-Class-Mages",
+    rarity: "Rare",
+    image: "/images/factions/first-class-mages.webp",
+  },
+  {
+    name: "Continental-Magic-Association",
+    rarity: "Common",
+    image: "/images/factions/continental-magic-association.webp",
+  },
+  {
+    name: "Stardust-Crusaders",
+    rarity: "Rare",
+    image: "/images/factions/stardust-crusaders.webp",
+  },
+  {
+    name: "Passione",
+    rarity: "Common",
+    image: "/images/factions/passione.webp",
+  },
+  {
+    name: "Yorozuya",
+    rarity: "Common",
+    image: "/images/factions/yorozuya.webp",
+  },
+  {
+    name: "Shinsengumi",
+    rarity: "Common",
+    image: "/images/factions/shinsengumi.webp",
+  },
+  {
+    name: "Band-of-the-Hawk",
+    rarity: "Common",
+    image: "/images/factions/band-of-the-hawk.webp",
+  },
+  {
+    name: "Holy-Iron-Chain-Knights",
+    rarity: "Common",
+    image: "/images/factions/holy-iron-chain-knights.webp",
+  },
+  {
+    name: "BKA-Federal-Police",
+    rarity: "Common",
+    image: "/images/factions/bka-federal-police.webp",
+  },
+  {
+    name: "511-Kinderheim",
+    rarity: "Common",
+    image: "/images/factions/511-kinderheim.webp",
+  },
+  {
+    name: "Z-Fighters",
+    rarity: "Legendary",
+    image: "/images/factions/z-fighters.webp",
+  },
+  {
+    name: "Galactic-Patrol",
+    rarity: "Rare",
+    image: "/images/factions/galactic-patrol.webp",
+  },
+  {
+    name: "Jomsvikings",
+    rarity: "Common",
+    image: "/images/factions/jomsvikings.webp",
+  },
+  {
+    name: "Ketils-Farm-Workers",
+    rarity: "Common",
+    image: "/images/factions/ketils-farm-workers.webp",
+  },
+  {
+    name: "Fairy-Tail-Guild",
+    rarity: "Epic",
+    image: "/images/factions/fairy-tail-guild.webp",
+  },
+  {
+    name: "Sabertooth-Guild",
+    rarity: "Common",
+    image: "/images/factions/sabertooth-guild.webp",
+  },
+  {
+    name: "Black-Bulls",
+    rarity: "Rare",
+    image: "/images/factions/black-bulls.webp",
+  },
+  {
+    name: "Golden-Dawn",
+    rarity: "Common",
+    image: "/images/factions/golden-dawn.webp",
+  },
+  {
+    name: "Team-Urameshi",
+    rarity: "Rare",
+    image: "/images/factions/team-urameshi.webp",
+  },
+  {
+    name: "Spirit-Detective-Office",
+    rarity: "Common",
+    image: "/images/factions/spirit-detective-office.webp",
+  },
+  {
+    name: "Inuyashas-Group",
+    rarity: "Common",
+    image: "/images/factions/inuyashas-group.webp",
+  },
+  {
+    name: "Band-of-Seven",
+    rarity: "Common",
+    image: "/images/factions/band-of-seven.webp",
+  },
+  {
+    name: "Ahjin-Guild",
+    rarity: "Rare",
+    image: "/images/factions/ahjin-guild.webp",
+  },
+  {
+    name: "Korean-Hunters-Association",
+    rarity: "Common",
+    image: "/images/factions/korean-hunters-association.webp",
+  },
+  {
+    name: "Yamada-Asaemon-Clan",
+    rarity: "Common",
+    image: "/images/factions/yamada-asaemon-clan.webp",
+  },
+  {
+    name: "Iwagakure-Shinobi",
+    rarity: "Common",
+    image: "/images/factions/iwagakure-shinobi.webp",
+  },
+  {
+    name: "Team-Momo-and-Okarun",
+    rarity: "Common",
+    image: "/images/factions/team-momo-and-okarun.webp",
+  },
+  {
+    name: "Kito-Family",
+    rarity: "Common",
+    image: "/images/factions/kito-family.webp",
+  },
+  {
+    name: "Public-Safety-Bureau",
+    rarity: "Common",
+    image: "/images/factions/public-safety-bureau.webp",
+  },
+  {
+    name: "Sibyl-System-Enforcers",
+    rarity: "Common",
+    image: "/images/factions/sibyl-system-enforcers.webp",
+  },
+  {
+    name: "Great-Tomb-of-Nazarick",
+    rarity: "Epic",
+    image: "/images/factions/great-tomb-of-nazarick.webp",
+  },
+  {
+    name: "E-Rantel-Adventurers-Guild",
+    rarity: "Common",
+    image: "/images/factions/e-rantel-adventurers-guild.webp",
+  },
+  {
+    name: "Shield-Hero-Party",
+    rarity: "Common",
+    image: "/images/factions/shield-hero-party.webp",
+  },
+  {
+    name: "Melromarc-Royal-Army",
+    rarity: "Common",
+    image: "/images/factions/melromarc-royal-army.webp",
   },
 ];
--- data/origins.ts
+++ data/origins.ts
@@ -2,33 +2,403 @@ import type { GameOption } from "@/types/game";
 
 export const origins: GameOption[] = [
   {
-    name: "Konoha",
-    rarity: "Legendary",
-    image: "/images/origins/konoha.webp",
+    name: "Shiganshina-District",
+    rarity: "Common",
+    image: "/images/origins/shiganshina-district.webp",
   },
   {
-    name: "Shiganshina",
-    rarity: "Epic",
-    image: "/images/origins/shiganshina.webp",
+    name: "Trost-District",
+    rarity: "Common",
+    image: "/images/origins/trost-district.webp",
   },
   {
-    name: "Soul Society",
-    rarity: "Epic",
-    image: "/images/origins/soul-society.webp",
-  },
-  {
-    name: "Clover Kingdom",
+    name: "Tokyo-Kanto",
     rarity: "Rare",
-    image: "/images/origins/clover-kingdom.webp",
+    image: "/images/origins/tokyo-kanto.webp",
   },
   {
-    name: "Abandoned Island",
-    rarity: "Common",
-    image: "/images/origins/abandoned-island.webp",
+    name: "Kansai-Osaka-Region",
+    rarity: "Rare",
+    image: "/images/origins/kansai-osaka-region.webp",
   },
   {
-    name: "Underground City",
+    name: "Resembool",
+    rarity: "Rare",
+    image: "/images/origins/resembool.webp",
+  },
+  {
+    name: "Ishval",
     rarity: "Common",
-    image: "/images/origins/underground-city.webp",
+    image: "/images/origins/ishval.webp",
+  },
+  {
+    name: "City-Z",
+    rarity: "Common",
+    image: "/images/origins/city-z.webp",
+  },
+  {
+    name: "City-A",
+    rarity: "Common",
+    image: "/images/origins/city-a.webp",
+  },
+  {
+    name: "Asakusa-Tokyo",
+    rarity: "Common",
+    image: "/images/origins/asakusa-tokyo.webp",
+  },
+  {
+    name: "Kumotori-Mountain-Region",
+    rarity: "Common",
+    image: "/images/origins/kumotori-mountain-region.webp",
+  },
+  {
+    name: "Musutafu",
+    rarity: "Epic",
+    image: "/images/origins/musutafu.webp",
+  },
+  {
+    name: "Tatooin-Station-Shopping-District",
+    rarity: "Common",
+    image: "/images/origins/tatooin-station-shopping-district.webp",
+  },
+  {
+    name: "Tokyo",
+    rarity: "Epic",
+    image: "/images/origins/tokyo.webp",
+  },
+  {
+    name: "Aincrad-Town-of-Beginnings",
+    rarity: "Common",
+    image: "/images/origins/aincrad-town-of-beginnings.webp",
+  },
+  {
+    name: "Meteor-City",
+    rarity: "Common",
+    image: "/images/origins/meteor-city.webp",
+  },
+  {
+    name: "Whale-Island",
+    rarity: "Rare",
+    image: "/images/origins/whale-island.webp",
+  },
+  {
+    name: "Konohagakure",
+    rarity: "Epic",
+    image: "/images/origins/konohagakure.webp",
+  },
+  {
+    name: "Kirigakure",
+    rarity: "Common",
+    image: "/images/origins/kirigakure.webp",
+  },
+  {
+    name: "Tokyo-20th-Ward",
+    rarity: "Common",
+    image: "/images/origins/tokyo-20th-ward.webp",
+  },
+  {
+    name: "Tokyo-24th-Ward-Underground",
+    rarity: "Common",
+    image: "/images/origins/tokyo-24th-ward-underground.webp",
+  },
+  {
+    name: "Tokyo-Jujutsu-Zone",
+    rarity: "Common",
+    image: "/images/origins/tokyo-jujutsu-zone.webp",
+  },
+  {
+    name: "Sendai",
+    rarity: "Common",
+    image: "/images/origins/sendai.webp",
+  },
+  {
+    name: "Foosha-Village",
+    rarity: "Rare",
+    image: "/images/origins/foosha-village.webp",
+  },
+  {
+    name: "Sabaody-Archipelago",
+    rarity: "Common",
+    image: "/images/origins/sabaody-archipelago.webp",
+  },
+  {
+    name: "Tokyo-Settlement-Area-11",
+    rarity: "Common",
+    image: "/images/origins/tokyo-settlement-area-11.webp",
+  },
+  {
+    name: "Pendragon",
+    rarity: "Rare",
+    image: "/images/origins/pendragon.webp",
+  },
+  {
+    name: "Lugunica-Slums",
+    rarity: "Common",
+    image: "/images/origins/lugunica-slums.webp",
+  },
+  {
+    name: "Lugunica-Royal-Capital",
+    rarity: "Common",
+    image: "/images/origins/lugunica-royal-capital.webp",
+  },
+  {
+    name: "Seasoning-City",
+    rarity: "Epic",
+    image: "/images/origins/seasoning-city.webp",
+  },
+  {
+    name: "Claw-Esper-Territory",
+    rarity: "Common",
+    image: "/images/origins/claw-esper-territory.webp",
+  },
+  {
+    name: "Tokyo-Near-Shore",
+    rarity: "Rare",
+    image: "/images/origins/tokyo-near-shore.webp",
+  },
+  {
+    name: "Far-Shore",
+    rarity: "Common",
+    image: "/images/origins/far-shore.webp",
+  },
+  {
+    name: "Imperial-Capital",
+    rarity: "Common",
+    image: "/images/origins/imperial-capital.webp",
+  },
+  {
+    name: "Empire-Border-Villages",
+    rarity: "Common",
+    image: "/images/origins/empire-border-villages.webp",
+  },
+  {
+    name: "Kunugigaoka-City",
+    rarity: "Epic",
+    image: "/images/origins/kunugigaoka-city.webp",
+  },
+  {
+    name: "Class-3-E-Mountain-Campus",
+    rarity: "Rare",
+    image: "/images/origins/class-3-e-mountain-campus.webp",
+  },
+  {
+    name: "Karakura-Town",
+    rarity: "Common",
+    image: "/images/origins/karakura-town.webp",
+  },
+  {
+    name: "Rukongai",
+    rarity: "Common",
+    image: "/images/origins/rukongai.webp",
+  },
+  {
+    name: "Liones",
+    rarity: "Common",
+    image: "/images/origins/liones.webp",
+  },
+  {
+    name: "Danafor",
+    rarity: "Common",
+    image: "/images/origins/danafor.webp",
+  },
+  {
+    name: "Grace-Field-House",
+    rarity: "Common",
+    image: "/images/origins/grace-field-house.webp",
+  },
+  {
+    name: "Goldy-Pond",
+    rarity: "Common",
+    image: "/images/origins/goldy-pond.webp",
+  },
+  {
+    name: "True-Cross-Academy-Town",
+    rarity: "Epic",
+    image: "/images/origins/true-cross-academy-town.webp",
+  },
+  {
+    name: "Kyoto",
+    rarity: "Rare",
+    image: "/images/origins/kyoto.webp",
+  },
+  {
+    name: "Tokyo-Devil-Hunter-Zone",
+    rarity: "Common",
+    image: "/images/origins/tokyo-devil-hunter-zone.webp",
+  },
+  {
+    name: "Rural-Japan",
+    rarity: "Common",
+    image: "/images/origins/rural-japan.webp",
+  },
+  {
+    name: "Central-Kingdoms-Capital-Region",
+    rarity: "Epic",
+    image: "/images/origins/central-kingdoms-capital-region.webp",
+  },
+  {
+    name: "Northern-Lands",
+    rarity: "Common",
+    image: "/images/origins/northern-lands.webp",
+  },
+  {
+    name: "Morioh",
+    rarity: "Rare",
+    image: "/images/origins/morioh.webp",
+  },
+  {
+    name: "Cairo",
+    rarity: "Common",
+    image: "/images/origins/cairo.webp",
+  },
+  {
+    name: "Edo",
+    rarity: "Rare",
+    image: "/images/origins/edo.webp",
+  },
+  {
+    name: "Yoshiwara",
+    rarity: "Common",
+    image: "/images/origins/yoshiwara.webp",
+  },
+  {
+    name: "Midland",
+    rarity: "Common",
+    image: "/images/origins/midland.webp",
+  },
+  {
+    name: "Elfhelm-Skellig",
+    rarity: "Epic",
+    image: "/images/origins/elfhelm-skellig.webp",
+  },
+  {
+    name: "Dusseldorf",
+    rarity: "Legendary",
+    image: "/images/origins/dusseldorf.webp",
+  },
+  {
+    name: "Prague",
+    rarity: "Rare",
+    image: "/images/origins/prague.webp",
+  },
+  {
+    name: "West-City",
+    rarity: "Legendary",
+    image: "/images/origins/west-city.webp",
+  },
+  {
+    name: "Mount-Paozu",
+    rarity: "Rare",
+    image: "/images/origins/mount-paozu.webp",
+  },
+  {
+    name: "Iceland",
+    rarity: "Rare",
+    image: "/images/origins/iceland.webp",
+  },
+  {
+    name: "Danelaw-England",
+    rarity: "Common",
+    image: "/images/origins/danelaw-england.webp",
+  },
+  {
+    name: "Magnolia",
+    rarity: "Epic",
+    image: "/images/origins/magnolia.webp",
+  },
+  {
+    name: "Galuna-Island",
+    rarity: "Common",
+    image: "/images/origins/galuna-island.webp",
+  },
+  {
+    name: "Hage-Village",
+    rarity: "Common",
+    image: "/images/origins/hage-village.webp",
+  },
+  {
+    name: "Clover-Kingdom-Royal-Capital",
+    rarity: "Common",
+    image: "/images/origins/clover-kingdom-royal-capital.webp",
+  },
+  {
+    name: "Sarayashiki-City",
+    rarity: "Rare",
+    image: "/images/origins/sarayashiki-city.webp",
+  },
+  {
+    name: "Demon-World-Borderlands",
+    rarity: "Common",
+    image: "/images/origins/demon-world-borderlands.webp",
+  },
+  {
+    name: "Feudal-Japan-Musashi-Province",
+    rarity: "Common",
+    image: "/images/origins/feudal-japan-musashi-province.webp",
+  },
+  {
+    name: "Modern-Tokyo",
+    rarity: "Epic",
+    image: "/images/origins/modern-tokyo.webp",
+  },
+  {
+    name: "Seoul",
+    rarity: "Rare",
+    image: "/images/origins/seoul.webp",
+  },
+  {
+    name: "Busan-Jeju-Region",
+    rarity: "Common",
+    image: "/images/origins/busan-jeju-region.webp",
+  },
+  {
+    name: "Edo-Shogunate",
+    rarity: "Common",
+    image: "/images/origins/edo-shogunate.webp",
+  },
+  {
+    name: "Kotaku-Shinsenkyo",
+    rarity: "Common",
+    image: "/images/origins/kotaku-shinsenkyo.webp",
+  },
+  {
+    name: "Kamigoe-City",
+    rarity: "Rare",
+    image: "/images/origins/kamigoe-city.webp",
+  },
+  {
+    name: "Kami-High-Area",
+    rarity: "Common",
+    image: "/images/origins/kami-high-area.webp",
+  },
+  {
+    name: "Tokyo-Sibyl-Zone",
+    rarity: "Rare",
+    image: "/images/origins/tokyo-sibyl-zone.webp",
+  },
+  {
+    name: "Non-Sibyl-Outlands",
+    rarity: "Common",
+    image: "/images/origins/non-sibyl-outlands.webp",
+  },
+  {
+    name: "E-Rantel",
+    rarity: "Common",
+    image: "/images/origins/e-rantel.webp",
+  },
+  {
+    name: "Carne-Village",
+    rarity: "Rare",
+    image: "/images/origins/carne-village.webp",
+  },
+  {
+    name: "Melromarc",
+    rarity: "Common",
+    image: "/images/origins/melromarc.webp",
+  },
+  {
+    name: "Siltvelt",
+    rarity: "Rare",
+    image: "/images/origins/siltvelt.webp",
   },
 ];
